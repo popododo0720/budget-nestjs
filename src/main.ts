2175bd9ecc09f0da446ba5adc6a15d1005c50c95
popododo0720 <popododo0720@naver.com>
2024-08-14 17:52:40 +0900

    cors setting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS setting
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,OPTIONS',
+    credentials: true,
+  });
+
+  // 기본 라우트 /api 로 설정
   app.setGlobalPrefix('api');
   await app.listen(5000);
 }
